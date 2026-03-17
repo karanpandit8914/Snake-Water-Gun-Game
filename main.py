d34777c85076e8742146a97d8cb6b0d5ed83dd7a
Karan Pandit <karanpanditvlogs2468@gmail.com>
2026-03-17 21:41:10 +0530

    added comments & added no. of draw matches

--- main.py
+++ main.py
@@ -9,24 +9,34 @@ import random
 #creating variable to count scores of your and computer
 yourscore = 0
 compscore = 0
+draw = 0
 
 #creating loop for continous play without restart  : y = yes and n = no
-
 replay = "y"
 
 while (replay == "y"):
+
+    #to choose random numbers by computer
     computer = random.choice ([-1, 0, 1])
+    #to input user's choice
     youstr = input("Enter your choice (s, w, g) : ")
+    
+    #uses dictonary to convert string to number and no. to string
     youdict = {"s": 1, "w": -1, "g": 0}
     reversedict = {1 : "Snake" , 0 : "Gun" , -1 : "Water"}
 
-    you = youdict[youstr]
+    you = youdict[youstr]  #to convert string to number for processing which is choosen by you
 
-    print (f"You Choose : {reversedict[you]} \nComputer Choose : {reversedict[computer]}")
+    #It Print your choice and computer choice every time during play
+    print (f"You Choose : {reversedict[you]} \nComputer Choose : {reversedict[computer]}\n")
 
+
+    #It show draw when both choice are same
     if (computer == you):
+        draw += 1
         print ("Draw")
 
+    #These conditional staements decides you win or lose!
     else:
         if (computer == -1 and you == 1):
             yourscore += 1
@@ -56,8 +66,10 @@ while (replay == "y"):
             print ("Something Went Wrong!")
 
 
+    #This decides you play continuous or not (program termination)!
     print ("\n")
     replay = input ("Want to replay? (y/n) : ")
 
+#During terminating of program it prints the score of Your & Computer and also print no. of draw
 else :
-    print (f"\nYour Score : {yourscore} \nComputer Score : {compscore}")
+    print (f"\nYour Score : {yourscore} \nComputer Score : {compscore} \nNo. of Draw Matches : {draw}")
